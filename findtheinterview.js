30a4ef37e10d4a04488e596845eafda56e0246b2
sharad23 <baidyasharad@gmail.com>
2022-02-23 11:01:10 +0400

    ok

---
+++ findtheinterview.js
@@ -0,0 +1,9 @@
+// asked in deliveroo
+// find the empty time slots 
+// check the time is available or not
+const a = [[0900, 1000], [1200, 1300]]
+const myTime = [1100, 1130]
+
+for (let i=0; i < a.length; i++) {
+    
+}
--- minimumplatforms.js
+++ minimumplatforms.js
@@ -4,8 +4,8 @@
 // const arr = [0900, 1100, 1235]
 // const dep = [1000, 1200, 1240]
 
-const arr = [800, 815, 910]
-const dep = [900, 1015, 1000]
+// const arr = [800, 815, 910]
+// const dep = [900, 1015, 1000]
 
 // let minimum_platforms = 1
 // for (let i =0; i < arr.length; i++) {
@@ -38,3 +38,27 @@ const dep = [900, 1015, 1000]
 // console.log(minimum_platforms)
 
 // efficeint apprach
+const arr = [800, 815, 910]
+const dep = [900, 1015, 1000]
+
+i = 0
+j = 0
+min_platform = 0
+platform = 0
+while (i < arr.length && j < dep.length) {
+    if (arr[i] < dep[j]) {
+        platform += 1
+        i += 1
+    }
+
+    if (dep[j] < arr[i]) {
+        platform -= 1
+        j += 1
+    }
+
+    if (platform > min_platform) {
+        min_platform = platform
+    }
+}
+
+console.log(min_platform)
